2bf13798c6abe83c502cfe33cbe4022bb57e0d5f
alvinkcs <pklichun@gmail.com>
2024-11-26 17:51:17 +0900

    implement flexible selection of test with temp chmaber

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-# print('hello world')
 import pyvisa
 import matplotlib.pyplot as plt
 import time
 import os
 import pyserial.espec
-# import visa
 rm = pyvisa.ResourceManager()
 print(rm)
 print(rm.list_resources())
 
 x = [] # saving x-values for plotting graph
 y = [] # saving y-values for plotting graph
-# z = [0,-1,-2,-3,-4,-5]
 
 measure_inst = rm.open_resource('GPIB0::22::INSTR') # 34401
 dcSupply_inst = rm.open_resource('GPIB0::10::INSTR') # 8200
@@ -31,18 +28,18 @@ txt_file_index = 1
 while (os.path.isfile('testrun%i.txt'%(txt_file_index)) == True):
     txt_file_index += 1
 
-# startV = input('Vds start voltage:')
-# finalV = input('Vds final voltage:')
-# diff = float(input('voltage difference in each step:'))
-# delay_time = float(input('delay time in each for cooling down:'))
-startV = 0
-finalV = 0
-diff = 0
-delay_time = 0
+# startV = 0
+# finalV = 0
+# diff = 0
+# delay_time = 0
+vgs_startV = 0.0
+vgs_finalV = 0.0
+vgs_diff = 0.0
+vds_value = 0.0
+delay_time = 0.0
 
-def run_measurement(voltage):
+def vds_increment(voltage=0.0,startV=0.0,finalV=0.0,diff=0.0,delay_time=0.0):
 
-    # voltage = z[iteration]
     # voltage = current_vgs
     if (voltage >= 0):
         inst_33120A.write('APPL:DC DEF, DEF, +%s' %(str(voltage)))
@@ -112,35 +109,38 @@ def fixed_vds(vgs_startV=0,vgs_finalV=0,vgs_diff=0,vds_value=0,delay_time=0):
         current_vgs += vgs_diff
         delay(delay_time)
 
-def vds_test_with_diff_vgs():
-    global startV, finalV, diff, delay_time
-    startV = input('Vds start voltage:')
-    finalV = input('Vds final voltage:')
-    diff = float(input('voltage difference in each step:'))
-    delay_time = float(input('delay time in each for cooling down:'))
+def vds_test_with_diff_vgs(temp=-273):
+    # global startV, finalV, diff, delay_time
+    # startV = float(input('Vds start voltage:'))
+    # finalV = float(input('Vds final voltage:'))
+    # diff = float(input('voltage difference in each step:'))
+    # delay_time = float(input('delay time in each for cooling down:'))
 
-    vgs_startV = float(input('Vgs start voltage:'))
-    vgs_finalV = float(input('Vgs final voltage:'))
-    vgs_diff = float(input('Vgs voltage difference in each step:'))
+    # vgs_startV = float(input('Vgs start voltage:'))
+    # vgs_finalV = float(input('Vgs final voltage:'))
+    # vgs_diff = float(input('Vgs voltage difference in each step:'))
     vgs_iterations = int((vgs_finalV - vgs_startV)/vgs_diff + 1)
     current_vgs = vgs_startV
     for j in range(vgs_iterations):
-        run_measurement(current_vgs)
+        vds_increment(current_vgs, startV, finalV, diff, delay_time)
         global x,y
-        plt.plot(x,y, label = 'Vgs = %iV'%(j))
+        if (temp == -273):
+            plt.plot(x,y, label = 'Vgs = %iV'%(j))
+        else:
+            plt.plot(x,y, label = 'Vgs = %iV with temp = %f'%(j,temp))
         x = []
         y = []
         current_vgs += vgs_diff
 
     plt.legend() # to show the label indicators
 
-def vgs_test_with_fixed_vds():
-    fixed_vds(vgs_startV=0,vgs_finalV=-5,vgs_diff=-0.1,vds_value=-5,delay_time=0.5)
+def vgs_test_with_fixed_vds(vgs_startV=0.0,vgs_finalV=0.0,vgs_diff=0.0,vds_value=0.0,delay_time=0.0):
     global x,y
+    fixed_vds(vgs_startV,vgs_finalV,vgs_diff,vds_value,delay_time)
     plt.plot(x,y)
     dcSupply_inst.write('V1+%f' %(0/10.0)) # V1+0.1000 = 0.1x10^1 = 1V
 
-def integrated_test_with_temp(temp=20):
+def integrated_test_with_temp(temp=20,test_choice=0):
     pyserial.espec.run_test(temp)
     print('wait 1min')
     time.sleep(60)
@@ -153,34 +153,76 @@ def integrated_test_with_temp(temp=20):
         else:
             print('wait 30sec')
             time.sleep(30)
-    print('starting vgs_test_with_fixed_vds')
-    vgs_test_with_fixed_vds()
     global x,y
-    plt.plot(x,y, label = 'Temp = %f'%(temp))
+    if (test_choice == 0):
+        print('starting vds_test_with_diff_vgs')
+        vds_test_with_diff_vgs()
+    elif (test_choice == 1):
+        print('starting vgs_test_with_fixed_vds')
+        vgs_test_with_fixed_vds(vgs_startV,vgs_finalV,vgs_diff,vds_value,delay_time)
+        plt.plot(x,y, label = 'Temp = %f'%(temp))
     x = []
     y = []
     plt.legend()
 
 choice = int(input('Please select a test below\nVds test with diff Vgs (0)\nVgs test with fixed Vds (1)\nTest with diff temp. (2)\ninput 0 or 1 or 2:'))
 if (choice == 0):
+    startV = float(input('Vds start voltage:'))
+    finalV = float(input('Vds final voltage:'))
+    diff = float(input('voltage difference in each step:'))
+    delay_time = float(input('delay time in each for cooling down:'))
+
+    vgs_startV = float(input('Vgs start voltage:'))
+    vgs_finalV = float(input('Vgs final voltage:'))
+    vgs_diff = float(input('Vgs voltage difference in each step:'))
     vds_test_with_diff_vgs()
+    plt.xlabel('VDS[V]')
 elif (choice == 1):
-    vgs_test_with_fixed_vds()
+    vgs_startV = float(input('Vgs start voltage:'))
+    vgs_finalV = float(input('Vgs final voltage:'))
+    vgs_diff = float(input('Vgs voltage difference in each step:'))
+    vds_value = float(input('Vds voltage:'))
+    delay_time = float(input('delay time in each step for cooling down:'))
+    vgs_test_with_fixed_vds(vgs_startV,vgs_finalV,vgs_diff,vds_value,delay_time)
+    plt.xlabel('VGS[V]')
 elif (choice == 2):
-    integrated_test_with_temp(50)
-    integrated_test_with_temp(20)
+    test_choice = int(input('Please select a test with temp below\nVds test with diff Vgs (0)\nVgs test with fixed Vds (1)\ninput 0 or 1:'))
+    if (test_choice == 0):
+        startV = float(input('Vds start voltage:'))
+        finalV = float(input('Vds final voltage:'))
+        diff = float(input('voltage difference in each step:'))
+        delay_time = float(input('delay time in each for cooling down:'))
+
+        vgs_startV = float(input('Vgs start voltage:'))
+        vgs_finalV = float(input('Vgs final voltage:'))
+        vgs_diff = float(input('Vgs voltage difference in each step:'))
+        integrated_test_with_temp(50,test_choice)
+        integrated_test_with_temp(20,test_choice)
+        plt.xlabel('VDS[V]')
+    elif (test_choice == 1):
+        vgs_startV = float(input('Vgs start voltage:'))
+        vgs_finalV = float(input('Vgs final voltage:'))
+        vgs_diff = float(input('Vgs voltage difference in each step:'))
+        vds_value = float(input('Vds voltage:'))
+        delay_time = float(input('delay time in each step for cooling down:'))
+        integrated_test_with_temp(50,test_choice)
+        integrated_test_with_temp(20,test_choice)
+        plt.xlabel('VGS[V]')
+    else:
+        print('incorrect input')
 else:
     print('incorrect input')
 
 inst_33120A.write('APPL:DC DEF, DEF, +%s' %('0.0'))
 print('Done')
-plt.xlabel('Voltage supply')
-plt.ylabel('current measured')
+# plt.xlabel('VDS[V] or VGS[V]')
+plt.ylabel('ID[A]')
 plt.title('current against voltage measurement')
 # plt.legend() # to show the label indicators
+plt.show()
 saving_picture = input('save the picture? (y/N):')
 if (saving_picture == 'y'):
     picture_name = input('picture name:')
     picture_name += '.png'
     plt.savefig(picture_name)
-plt.show()
+# plt.show()
